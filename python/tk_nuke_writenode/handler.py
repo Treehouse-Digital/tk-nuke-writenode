0cbb18edcaa84d2162204cfb2e111360c939ea29
Joseph Yu <joseph.yu@treehousevfx.studio>
2026-09-22 10:57:16 +0100

    updated info.yml

--- python/tk_nuke_writenode/handler.py
+++ python/tk_nuke_writenode/handler.py
@@ -1907,8 +1907,7 @@ class TankWriteNodeHandler(object):
         # update with file extension if required
         if (
             (profile := self.__get_node_profile_settings(node))
-            and isinstance(ext_field := profile.get("ext_field", "ext"), str)
-            and ext_field
+            and (ext_field := str(profile.get("ext_field") or "ext"))
             and ext_field in render_template.keys
             and ext_field not in fields
             and (file_type := profile.get("file_type"))
